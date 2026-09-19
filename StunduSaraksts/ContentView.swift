cb8e607f5d8f626dc73bd3c70dae8eea9633037b
Zvejniekks <tzvejnieks09@gmail.com>
2026-09-19 15:22:00 +0300

    Create ContentView.swift

---
+++ StunduSaraksts/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Mans stundu saraksts")
+                .font(.title)
+            Text("Pirmais build strādā! 🎉")
+        }
+        .padding()
+    }
+}
